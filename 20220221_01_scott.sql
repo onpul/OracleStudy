301b4db4292ded8377c67e26f94d8a12678dda89
onpul <chmj072@gmail.com>
2022-02-21 12:05:27 +0900

    20220221 오라클 4일 차

---
+++ 20220221_01_scott.sql
@@ -0,0 +1,422 @@
+SELECT USER
+FROM DUAL;
+--==>> SCOTT
+
+
+--※ BETWEEN ⓐ AND ⓑ 는 날짜형, 숫자형, 문자형 데이터 모두에 적용된다.
+--   단, 문자형일 경우 아스키코드 순서를 따르기 때문에 (사전식 배열)
+--   대문자가 앞쪽에 위치하고 소문자가 뒤쪽에 위치한다.
+--   또한, BETWEEN ⓐ AND ⓑ는 해당 구문이 수행되는 시점에서
+--   오라클 내부적으로는 부등호 연산자의 형태로 바뀌어 연산 처리된다.
+
+SELECT ASCII('A') "COL1", ASCII('B') "COL2", ASCII('a') "COL3", ASCII('b') "COL4"
+FROM DUAL;
+--==>> 65	66	97	98
+
+
+SELECT ENAME, JOB, SAL
+FROM TBL_EMP
+WHERE JOB = 'SALESMAN'
+   OR JOB = 'CLERK';
+--==>>
+/*
+SMITH	CLERK	     800
+ALLEN	SALESMAN	1600
+WARD	SALESMAN	1250
+MARTIN	SALESMAN	1250
+TURNER	SALESMAN	1500
+ADAMS	CLERK	    1100
+JAMES	CLERK	     950
+MILLER	CLERK   	1300
+호석이	SALESMAN	(null)
+문정이	SALESMAN	(null)
+*/
+   
+SELECT ENAME, JOB, SAL
+FROM TBL_EMP
+WHERE JOB IN('SALESMAN', 'CLERK'); -- 얘도 내부적으로는 위와 같이 실행됨
+--==>>
+/*
+SMITH	CLERK	     800
+ALLEN	SALESMAN	1600
+WARD	SALESMAN	1250
+MARTIN	SALESMAN	1250
+TURNER	SALESMAN	1500
+ADAMS	CLERK	    1100
+JAMES	CLERK	     950
+MILLER	CLERK   	1300
+호석이	SALESMAN	(null)
+문정이	SALESMAN	(null)
+*/
+
+SELECT ENAME, JOB, SAL
+FROM TBL_EMP
+WHERE JOB = ANY ('SALESMAN', 'CLERK'); -- 내부적으로는 OR논리연산 형태로 바뀌어 처리됨
+--==>>
+/*
+SMITH	CLERK	     800
+ALLEN	SALESMAN	1600
+WARD	SALESMAN	1250
+MARTIN	SALESMAN	1250
+TURNER	SALESMAN	1500
+ADAMS	CLERK	    1100
+JAMES	CLERK	     950
+MILLER	CLERK   	1300
+호석이	SALESMAN	(null)
+문정이	SALESMAN	(null)
+*/
+
+
+--※ 위의 3가지 유형의 쿼리문은 모두 같은 결과를 반환한다.
+--   하지만, 맨 위의 쿼리문(OR) 이 가장 빠르게 처리된다.
+--   물론 메모리에 대한 내용이 아니라 CPU 처리에 대한 내용이므로
+--   이 부분까지 감안하여 쿼리문을 구성하게 되는 경우는 많지 않다.
+--   → 『IN』 과 『=ANY』는 같은 연산자 효과를 가진다.
+--      이들 모두는 내부적으로 『OR』 구조로 변경되어 연산 처리된다.
+
+--------------------------------------------------------------------------------
+
+--○ 추가 실습 테이블 구성(TBL_SAWON)
+CREATE TABLE TBL_SAWON
+( SANO      NUMBER(4)
+, SANAME    VARCHAR2(30)
+, JUBUN     CHAR(13)
+, HIREDATE  DATE        DEFAULT SYSDATE
+, SAL       NUMBER(10)
+);
+--==>> Table TBL_SAWON이(가) 생성되었습니다.
+
+SELECT *
+FROM TBL_SAWON;
+--==>> 조회 결과 없음
+
+DESC TBL_SAWON;
+--==>>
+/*
+이름       널? 유형           
+-------- -- ------------ 
+SANO        NUMBER(4)    
+SANAME      VARCHAR2(30) 
+JUBUN       CHAR(13)     
+HIREDATE    DATE         
+SAL         NUMBER(10)
+*/
+
+--○ 생성된 테이블에 데이터 입력(TBL_SAWON)
+INSERT INTO TBL_SAWON(SANO, SANAME, JUBUN, HIREDATE, SAL)
+VALUES(1001, '김민성', '9707251234567', TO_DATE('2005-01-03', 'YYYY-MM-DD'), 3000);
+
+INSERT INTO TBL_SAWON(SANO, SANAME, JUBUN, HIREDATE, SAL)
+VALUES(1002, '서민지', '9505152234567', TO_DATE('1999-11-23', 'YYYY-MM-DD'), 4000);
+
+INSERT INTO TBL_SAWON(SANO, SANAME, JUBUN, HIREDATE, SAL)
+VALUES(1003, '이지연', '9905192234567', TO_DATE('2006-08-10', 'YYYY-MM-DD'), 3000);
+
+INSERT INTO TBL_SAWON(SANO, SANAME, JUBUN, HIREDATE, SAL)
+VALUES(1004, '이연주', '9508162234567', TO_DATE('2007-10-10', 'YYYY-MM-DD'), 4000);
+
+INSERT INTO TBL_SAWON(SANO, SANAME, JUBUN, HIREDATE, SAL)
+VALUES(1005, '오이삭', '9805161234567', TO_DATE('2007-10-10', 'YYYY-MM-DD'), 4000);
+
+INSERT INTO TBL_SAWON(SANO, SANAME, JUBUN, HIREDATE, SAL)
+VALUES(1006, '이현이', '8005132234567', TO_DATE('1999-10-10', 'YYYY-MM-DD'), 1000);
+
+INSERT INTO TBL_SAWON(SANO, SANAME, JUBUN, HIREDATE, SAL)
+VALUES(1007, '박한이', '0204053234567', TO_DATE('2010-10-10', 'YYYY-MM-DD'), 1000);
+
+INSERT INTO TBL_SAWON(SANO, SANAME, JUBUN, HIREDATE, SAL)
+VALUES(1008, '선동렬', '6803171234567', TO_DATE('1998-10-10', 'YYYY-MM-DD'), 1500);
+
+INSERT INTO TBL_SAWON(SANO, SANAME, JUBUN, HIREDATE, SAL)
+VALUES(1009, '선우용녀', '6912232234567', TO_DATE('1998-10-10', 'YYYY-MM-DD'), 1300);
+
+INSERT INTO TBL_SAWON(SANO, SANAME, JUBUN, HIREDATE, SAL)
+VALUES(1010, '선우선', '0303044234567', TO_DATE('2010-10-10', 'YYYY-MM-DD'), 1600);
+
+INSERT INTO TBL_SAWON(SANO, SANAME, JUBUN, HIREDATE, SAL)
+VALUES(1011, '남주혁', '0506073234567', TO_DATE('2012-10-10', 'YYYY-MM-DD'), 2600);
+
+INSERT INTO TBL_SAWON(SANO, SANAME, JUBUN, HIREDATE, SAL)
+VALUES(1012, '남궁민', '0208073234567', TO_DATE('2012-10-10', 'YYYY-MM-DD'), 2600);
+
+INSERT INTO TBL_SAWON(SANO, SANAME, JUBUN, HIREDATE, SAL)
+VALUES(1013, '남진', '6712121234567', TO_DATE('1998-10-10', 'YYYY-MM-DD'), 2200);
+
+INSERT INTO TBL_SAWON(SANO, SANAME, JUBUN, HIREDATE, SAL)
+VALUES(1014, '홍수민', '0005044234567', TO_DATE('2015-10-10', 'YYYY-MM-DD'), 5200);
+
+INSERT INTO TBL_SAWON(SANO, SANAME, JUBUN, HIREDATE, SAL)
+VALUES(1015, '임소민', '9711232234567', TO_DATE('2007-10-10', 'YYYY-MM-DD'), 5500);
+
+--==>> 1 행 이(가) 삽입되었습니다. * 15
+
+--○ 확인
+SELECT *
+FROM TBL_SAWON;
+--==>>
+/*
+1001	김민성     	9707251234567	2005-01-03	3000
+1002	서민지	    9505152234567	1999-11-23	4000
+1003	이지연	    9905192234567	2006-08-10	3000
+1004	이연주	    9508162234567	2007-10-10	4000
+1005	오이삭	    9805161234567	2007-10-10	4000
+1006	이현이	    8005132234567	1999-10-10	1000
+1007	박한이	    0204053234567	2010-10-10	1000
+1008	선동렬	    6803171234567	1998-10-10	1500
+1009	선우용녀	6912232234567	1998-10-10	1300
+1010	선우선	    0303044234567	2010-10-10	1600
+1011	남주혁	    0506073234567	2012-10-10	2600
+1012	남궁민	    0208073234567	2012-10-10	2600
+1013	남진	    6712121234567	1998-10-10	2200
+1014	홍수민	    0005044234567	2015-10-10	5200
+1015	임소민	    9711232234567	2007-10-10	5500
+*/
+
+--○ 커밋
+COMMIT;
+--==>> 커밋 완료.
+
+--○ TBL_SAWON 테이블에서 '이지연' 사원의 데이터를 조회한다.
+SELECT *
+FROM TBL_SAWON
+WHERE SANAME = '이지연';
+--==>> 1003	이지연	9905192234567	2006-08-10	3000
+
+SELECT *
+FROM TBL_SAWON
+WHERE SANAME LIKE '이지연'; -- ~와 같이, ~처럼 LIKE
+--==>> 1003	이지연	9905192234567	2006-08-10	3000
+
+--※ LIKE : 동사 → 좋아하다
+--          부사 → ~와 같이, ~처럼    CHECK~!!!
+
+--※ WHILD CARD(CHARACTER) → 『%』
+--   『LIKE』와 함께 사용되는 『%』는 모든 글자를 의미하고
+--   『LIKE』와 함께 사용되는 『_』는 아무 글자 한 개를 의미한다.
+
+--○ TBL_SAWON 테이블에서 성씨가 『김』씨인 사원의
+--   사원명, 주민번호, 급여 항목을 조회한다.
+SELECT *
+FROM TBL_SAWON
+WHERE SANAME = '김';
+--==>> 조회 결과 없음
+
+SELECT *
+FROM TBL_SAWON
+WHERE SANAME LIKE '김__'; -- 언더바 쓰려면 꼭 라이크랑 같이 쓰기
+--==>> 1001	김민성	9707251234567	2005-01-03	3000
+
+SELECT *
+FROM TBL_SAWON
+WHERE SANAME LIKE '김%'; -- 퍼센트 쓰려면 꼭 라이크랑 같이 쓰기
+--==>> 1001	김민성	9707251234567	2005-01-03	3000
+
+
+--○ TBL_SAWON 테이블에서 성씨가 『이』씨인 사원의
+--   사원명, 주민번호, 급여 항목을 조회한다.
+SELECT *
+FROM TBL_SAWON
+WHERE SANAME LIKE '이__';
+-- WHERE SANAME LIKE '이%';
+--==>>
+/*
+1003	이지연	9905192234567	2006-08-10	3000
+1004	이연주	9508162234567	2007-10-10	4000
+1006	이현이	8005132234567	1999-10-10	1000
+*/
+
+--○ TBL_SAWON 테이블에서 이름의 마지막 글자가 『민』인 사원의
+--   사원명, 주민번호, 급여 항목을 조회한다.
+SELECT SANAME "사원명", JUBUN "주민번호", SAL "급여"
+FROM TBL_SAWON
+WHERE SANAME LIKE '%민';
+--==>>
+/*
+남궁민	0208073234567	2600
+홍수민	0005044234567	5200
+임소민	9711232234567	5500
+*/
+
+SELECT SANAME "사원명", JUBUN "주민번호", SAL "급여"
+FROM TBL_SAWON
+WHERE SANAME LIKE '__민';
+--==>>
+/*
+남궁민	0208073234567	2600
+홍수민	0005044234567	5200
+임소민	9711232234567	5500
+*/
+
+--○ 추가 데이터 입력(TBL_SAWON)
+INSERT INTO TBL_SAWON(SANO, SANAME, JUBUN, HIREDATE, SAL)
+VALUES(1016, '이이경', '0603194234567', TO_DATE('2015-01-20', 'YYYY-MM-DD'), 1500);
+--==>> 1 행 이(가) 삽입되었습니다.
+
+--○ 확인
+SELECT *
+FROM TBL_SAWON;
+--==>>
+/*
+1001	김민성	    9707251234567	2005-01-03	3000
+1002	서민지	    9505152234567	1999-11-23	4000
+1003	이지연	    9905192234567	2006-08-10	3000
+1004	이연주	    9508162234567	2007-10-10	4000
+1005	오이삭	    9805161234567	2007-10-10	4000
+1006	이현이	    8005132234567	1999-10-10	1000
+1007	박한이	    0204053234567	2010-10-10	1000
+1008	선동렬	    6803171234567	1998-10-10	1500
+1009	선우용녀	6912232234567	1998-10-10	1300
+1010	선우선	    0303044234567	2010-10-10	1600
+1011	남주혁	    0506073234567	2012-10-10	2600
+1012	남궁민	    0208073234567	2012-10-10	2600
+1013	남진	    6712121234567	1998-10-10	2200
+1014	홍수민	    0005044234567	2015-10-10	5200
+1015	임소민	    9711232234567	2007-10-10	5500
+1016	이이경	    0603194234567	2015-01-20	1500
+*/
+
+--○ 커밋
+COMMIT;
+--==>> 커밋 완료.
+
+--○ TBL_SAWON 테이블에서 사원의 이름에 『이』라는 글자가
+--   하나라도 포함되어 있다면 그 사원의
+--   사원번호, 사원명, 급여 항목을 조회한다.
+SELECT SANO "사원번호", SANAME "사원명", SAL "급여"
+FROM TBL_SAWON
+WHERE SANAME LIKE '%이%'; -- 없는 것도 퍼센트 안에 포함되는군
+--==>>
+/*
+1003	이지연	3000
+1004	이연주	4000
+1005	오이삭	4000
+1006	이현이	1000
+1007	박한이	1000
+1016	이이경	1500
+*/
+
+--○ TBL_SAWON 테이블에서 사원의 이름에 『이』라는 글자가
+--   연속으로 두 번 들어있는 사원의
+--   사원번호, 사원명, 급여 항목을 조회한다.
+SELECT SANO "사원번호", SANAME "사원명", SAL "급여"
+FROM TBL_SAWON
+WHERE SANAME LIKE '%이이%';
+--==>>1016	이이경	1500
+
+
+--○ TBL_SAWON 테이블에서 사원의 이름에 『이』라는 글자가 두 번 들어있는 사원의
+--   사원번호, 사원명, 급여 항목을 조회한다.
+SELECT SANO "사원번호", SANAME "사원명", SAL "급여"
+FROM TBL_SAWON
+WHERE SANAME LIKE '%이%이%';
+--==>>
+/*
+1006	이현이	1000
+1016	이이경	1500
+*/
+
+--○ TBL_SAWON 테이블에서 사원 이름의 두 번째 글자가 『이』인 사원의
+--   사원번호, 사원명, 급여 항목을 조회한다.
+SELECT SANO "사원번호", SANAME "사원명", SAL "급여"
+FROM TBL_SAWON
+WHERE SANAME LIKE '_이%';
+--==>>
+/*
+1005	오이삭	4000
+1016	이이경	1500
+*/
+
+
+--○ TBL_SAWON 테이블에서 사원의 성씨가 『선』씨인 사원의
+--   사원명, 주민번호, 급여 항목을 조회한다.
+--   → 불가!!!!!!
+SELECT SANAME "사원명", JUBUN "주민번호", SAL "급여"
+FROM TBL_SAWON
+WHERE SANAME LIKE '선%';
+
+SELECT SANAME "사원명", JUBUN "주민번호", SAL "급여"
+FROM TBL_SAWON
+WHERE SANAME LIKE '남%';
+--==>> 성씨가 선우인지, 선인지 모르고 / 남궁인지 남인지 모름...
+
+--※ 데이터베이스 설계 과정에서
+--   성과 이름을 분리하여 처리해야 할 업무 계획이 있다면
+--   (지금 당장은 아니더라도...)
+--   테이블에서 성 컬럼과 이름 컬럼을 분리하여 구성해야 한다.
+
+
+--○ TBL_SAWON 테이블에서 여직원들의
+--   사원명, 주민번호, 급여 항목을 조회한다.
+DESC TBL_SAWON;
+--==>>
+/*
+이름       널? 유형           
+-------- -- ------------ 
+SANO        NUMBER(4)    
+SANAME      VARCHAR2(30) 
+JUBUN       CHAR(13)     
+HIREDATE    DATE         
+SAL         NUMBER(10)
+*/
+
+SELECT SANAME "사원명", JUBUN "주민번호", SAL "급여"
+FROM TBL_SAWON
+WHERE JUBUN LIKE '______2______' OR JUBUN LIKE '______4______';
+--==>>
+/*
+서민지	    9505152234567	4000
+이지연	    9905192234567	3000
+이연주	    9508162234567	4000
+이현이     	8005132234567	1000
+선우용녀	6912232234567	1300
+선우선     	0303044234567	1600
+홍수민	    0005044234567	5200
+임소민	    9711232234567	5500
+이이경	    0603194234567	1500
+*/
+
+
+--○ 테이블 생성(TBL_WATCH)
+CREATE TABLE TBL_WATCH
+( WATCH_NAME    VARCHAR2(20)
+, BIGO          VARCHAR2(100)
+);
+--==>> Table TBL_WATCH이(가) 생성되었습니다.
+
+--○ 데이터 입력(TBL_WATCH)
+INSERT INTO TBL_WATCH(WATCH_NAME, BIGO)
+VALUES('금시계', '순금 99.99% 함유된 최고급 시계');
+--==>> 1 행 이(가) 삽입되었습니다.
+
+INSERT INTO TBL_WATCH(WATCH_NAME, BIGO)
+VALUES('은시계', '고객 만족도 99.99점을 획득한 멋진 시계');
+--==>> 1 행 이(가) 삽입되었습니다.
+
+--○ 확인
+SELECT *
+FROM TBL_WATCH;
+--==>>
+/*
+금시계	순금 99.99% 함유된 최고급 시계
+은시계	고객 만족도 99.99점을 획득한 멋진 시계
+*/
+
+--○ 커밋
+COMMIT;
+--==>> 커밋 완료.
+
+
+--○ TBL_WATCH 테이블의 BIGO(비고) 컬럼에
+--   『99.99%』라는 글자가 포함된(들어있는) 행(레코드)의
+--   데이터를 조회한다.
+SELECT *
+FROM TBL_WATCH
+WHERE BIGO LIKE '%99.99%';
+--==>>
+
+
+
+
+
+
+
